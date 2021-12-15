ebca49c6a9fae38bad077adce3bd4532612794b3
Ruairidh Cavaye <ruairidh.cavaye@gmail.com>
2021-12-15 15:51:28 +0000

    update imports

--- routes/routes.py
+++ routes/routes.py
@@ -1,6 +1,6 @@
-from api import app, db
+from .. import app, db
 from flask import jsonify, request, Response
-from api.models.models import Recipe, User
+from ..models.models import Recipe, User
 from flask_jwt_extended import (
     create_access_token,
     jwt_required,
